c5acba2399a41f52e0ff446979eade38d43283c3
Gray Putnam <grayputnam@uchicago.edu>
2018-12-20 10:13:48 -0600

    Initial commit of redis plugin with hiredis included as a subdirectory. Is able to build as an mrb product.

---
+++ sbndaq-redis-plugin/redis_metric.cc
@@ -0,0 +1,120 @@
+// Author: Gray Putnam
+// Last Modified: 18 December 2018
+// An implementation of the MetricPlugin interface for Redis
+
+// TODO: error handling
+// TODO: check if user provided "name" is a valid stream name for Redis
+
+#include "fhiclcpp/fwd.h"
+#include "artdaq-utilities/Plugins/MetricMacros.hh"
+
+#include "hiredis/hiredis.h"
+#include "hiredis/async.h"
+
+namespace sbndaq {
+  void CallProcessRedisReply(redisAsyncContext *c, void *reply, void *plugin);
+
+  class RedisMetric : public artdaq::MetricPlugin {
+  private:
+    bool _verbose;
+    std::string _server_name;
+    unsigned _server_port;
+    unsigned _maxlen;
+    redisAsyncContext *_context;
+
+  public:
+    RedisMetric(fhicl::ParameterSet const& pset, std::string const& app_name): 
+      MetricPlugin(pset, app_name),
+      _verbose(pset.get<bool>("verbose",false)),
+      _context(NULL)
+    {
+      _server_name = pset.get<std::string>("hostname", "localhost");
+      _server_port = pset.get<unsigned>("port", 6379 /* default redis port */);
+      _maxlen = pset.get<unsigned>("maxlen", 1000); // TODO: should maxlen have a max by default? Would be good to 
+    }
+
+    virtual ~RedisMetric() {
+      if (_context != NULL) {
+        redisAsyncDisconnect(_context);
+      }
+    }
+
+    // TODO: implement
+    void ProcessRedisReturn(int retval) {}
+    // TODO: implement
+    void ProcessRedisReply(void *reply) {}
+    // TODO: implement
+    std::string ValidateRedisName(const std::string &name) { 
+      return name;
+    }
+
+    void stopMetrics_() {
+      if (_context != NULL) {
+        redisAsyncDisconnect(_context);
+      }
+    }
+
+    void startMetrics_() {
+      //_context = redisConnect(_server_name.c_str(), _server_port);
+      _context = redisAsyncConnect(_server_name.c_str(), _server_port);
+      if (_context == NULL || _context->err) {
+        if (_context) {
+          std::cerr << "Error: " << _context->errstr << std::endl;
+        }
+        else {
+          std::cerr << "Error: cannot allocate redis context." << std::endl;
+        }
+      }
+    }
+
+    void sendMetric_(const std::string &name, const std::string &value, const std::string &units) {
+      (void) units;
+      std::string redis_name = ValidateRedisName(name);
+      if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
+      int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * val %s", redis_name, _maxlen, value);
+      ProcessRedisReturn(ret);
+    }
+
+    void sendMetric_(const std::string &name, const int &value, const std::string &units) {
+      (void) units;
+      std::string redis_name = ValidateRedisName(name);
+      if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
+      int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * val %i", redis_name, _maxlen, value);
+      ProcessRedisReturn(ret);
+    }
+
+    void sendMetric_(const std::string &name, const double &value, const std::string &units) {
+      (void) units;
+      std::string redis_name = ValidateRedisName(name);
+      if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
+      int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * val %f", redis_name, _maxlen, value);
+      ProcessRedisReturn(ret);
+    }
+
+    void sendMetric_(const std::string &name, const float &value, const std::string &units) {
+      (void) units;
+      std::string redis_name = ValidateRedisName(name);
+      if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
+      int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * val %f", redis_name, _maxlen, value);
+      ProcessRedisReturn(ret);
+    }
+
+    void sendMetric_(const std::string &name, const unsigned long int &value, const std::string &units) {
+      (void) units;
+      std::string redis_name = ValidateRedisName(name);
+      if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
+      int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * val %u", redis_name, _maxlen, value);
+      ProcessRedisReturn(ret);
+    }
+  };
+
+  void CallProcessRedisReply(redisAsyncContext *c, void *reply, void *plugin) {
+    // "plugin" should be an instance of RedisMetric
+    ((RedisMetric *)plugin)->ProcessRedisReply(reply);
+  }
+
+
+} // end namespace sbndaq
+
+DEFINE_ARTDAQ_METRIC(sbndaq::RedisMetric)
+
