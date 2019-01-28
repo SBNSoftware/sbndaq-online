90db424b36d5a06c3fd02851ed8984b1e5a3edb7
Gray Putnam <grayputnam@uchicago.edu>
2019-01-28 11:06:57 -0600

    add options to buffer messages and to add a postfix to stream names

--- sbndaq-redis-plugin/redis_metric.cc
+++ sbndaq-redis-plugin/redis_metric.cc
@@ -18,9 +18,25 @@ namespace sbndaq {
   private:
     bool _verbose;
     std::string _server_name;
+    std::string _redis_key_postfix;
     unsigned _server_port;
     unsigned _maxlen;
+    unsigned _message_buffer_size;
+    unsigned _n_buffered_messages;
     redisContext *_context;
+
+
+    void newMessage() {
+      _n_buffered_messages += 1;
+      if (_n_buffered_messages >= _message_buffer_size) {
+        for (unsigned i = 0; i < _n_buffered_messages; i++) {
+          void *reply;
+          redisGetReply(_context, &reply);
+          ProcessRedisReply(reply);
+        }
+        _n_buffered_messages = 0;
+      }
+    }
 
   public:
     RedisMetric(fhicl::ParameterSet const& pset, std::string const& app_name): 
@@ -31,6 +47,9 @@ namespace sbndaq {
       _server_name = pset.get<std::string>("hostname", "localhost");
       _server_port = pset.get<unsigned>("port", 6379 /* default redis port */);
       _maxlen = pset.get<unsigned>("maxlen", 1000); // TODO: should maxlen have a max by default? Would be good to 
+      _message_buffer_size = pset.get<unsigned>("message_buffer_size", 1); // don't buffer by default
+      _n_buffered_messages = 0;
+      _redis_key_postfix = pset.get<std::string>("redis_key_postfix", "");
     }
 
     virtual ~RedisMetric() {
@@ -42,10 +61,14 @@ namespace sbndaq {
     // TODO: implement
     void ProcessRedisReturn(int retval) {}
     // TODO: implement
-    void ProcessRedisReply(void *reply) {}
+    void ProcessRedisReply(void *reply) {
+      freeReplyObject(reply);
+    }
     // TODO: implement
     std::string ValidateRedisName(const std::string &name) { 
-      std::string ret = name;
+      // add the post fix
+      std::string ret = name + _redis_key_postfix;
+      // remove the dots...
       ret.erase(std::remove(ret.begin(), ret.end(), '.'), ret.end());
       return ret;
     }
@@ -75,8 +98,8 @@ namespace sbndaq {
       if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
       //int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * dat %s", redis_name.c_str(), _maxlen, value);
       //ProcessRedisReturn(ret);
-      void *reply = redisCommand(_context, "XADD %s MAXLEN ~ %i * dat %s", redis_name.c_str(), _maxlen, value.c_str());
-      ProcessRedisReply(reply);
+      redisAppendCommand(_context, "XADD %s MAXLEN ~ %i * dat %s", redis_name.c_str(), _maxlen, value.c_str());
+      newMessage();
     }
 
     void sendMetric_(const std::string &name, const int &value, const std::string &units) {
@@ -85,8 +108,8 @@ namespace sbndaq {
       if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
       //int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * dat %i", redis_name.c_str(), _maxlen, value);
       //ProcessRedisReturn(ret);
-      void *reply = redisCommand(_context, "XADD %s MAXLEN ~ %i * dat %i", redis_name.c_str(), _maxlen, value);
-      ProcessRedisReply(reply);
+      redisAppendCommand(_context, "XADD %s MAXLEN ~ %i * dat %i", redis_name.c_str(), _maxlen, value);
+      newMessage();
     }
 
     void sendMetric_(const std::string &name, const double &value, const std::string &units) {
@@ -95,8 +118,8 @@ namespace sbndaq {
       if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
       //int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * dat %f", redis_name.c_str(), _maxlen, value);
       //ProcessRedisReturn(ret);
-      void *reply = redisCommand(_context, "XADD %s MAXLEN ~ %i * dat %f", redis_name.c_str(), _maxlen, value);
-      ProcessRedisReply(reply);
+      redisAppendCommand(_context, "XADD %s MAXLEN ~ %i * dat %f", redis_name.c_str(), _maxlen, value);
+      newMessage();
     }
 
     void sendMetric_(const std::string &name, const float &value, const std::string &units) {
@@ -105,8 +128,8 @@ namespace sbndaq {
       if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
       //int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * dat %f", redis_name.c_str(), _maxlen, value);
       //ProcessRedisReturn(ret);
-      void *reply = redisCommand(_context, "XADD %s MAXLEN ~ %i * dat %f", redis_name.c_str(), _maxlen, value);
-      ProcessRedisReply(reply);
+      redisAppendCommand(_context, "XADD %s MAXLEN ~ %i * dat %f", redis_name.c_str(), _maxlen, value);
+      newMessage();
     }
 
     void sendMetric_(const std::string &name, const unsigned long int &value, const std::string &units) {
@@ -115,8 +138,8 @@ namespace sbndaq {
       if (_verbose) std::cout << "Adding metric to stream: (" << redis_name << ") with value (" << value << ")" << std::endl;
       //int ret = redisAsyncCommand(_context, CallProcessRedisReply, this, "XADD %s MAXLEN ~ %i * dat %u", redis_name.c_str(), _maxlen, value);
       //ProcessRedisReturn(ret);
-      void *reply = redisCommand(_context, "XADD %s MAXLEN ~ %i * dat %u", redis_name.c_str(), _maxlen, value);
-      ProcessRedisReply(reply);
+      redisAppendCommand(_context, "XADD %s MAXLEN ~ %i * dat %u", redis_name.c_str(), _maxlen, value);
+      newMessage();
     }
   };
 
